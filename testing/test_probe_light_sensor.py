86534d84885213951e8040ac142a8823a2094505
ArVID220u <arvid.lunnemark@gmail.com>
2017-09-09 22:59:56 +0200

    Test probe touch with color sensor

---
+++ testing/test_probe_light_sensor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+import BrickPi as bp
+import time
+
+bp.BrickPiSetup()  # setup the serial port for communication
+
+color = bp.PORT_1
+
+#col = [None , "Black","Blue","Green","Yellow","Red","White" ]   #used for converting the color index to name
+
+bp.BrickPi.SensorType[color] = bp.TYPE_SENSOR_COLOR_RED
+
+bp.BrickPiSetupSensors()
+
+inn = False
+while True:
+    result = bp.BrickPiUpdateValues()  # Ask BrickPi to update values for sensors/motors 
+    if not result:
+        val = bp.BrickPi.Sensor[color]     #BrickPi.Sensor[PORT] stores the value obtained from sensor
+        if val > 140 and not inn:
+            inn = True
+            print("In!")
+        if val < 120 and inn:
+            inn = False
+            print("Out!")
+    time.sleep(.1)     # sleep for 100 m
