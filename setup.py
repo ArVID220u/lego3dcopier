9bd99747d5f3c3bc51c38074bcfc63a9adae0ae8
ArVID220u <arvid.lunnemark@gmail.com>
2017-09-23 16:52:16 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# setup file
+
+# SCANNER
+import BrickPi as bp
+
+start_button_port = bp.PORT_3
+rotate_base_port = bp.PORT_A
+slide_probe_port = bp.PORT_B
+eject_probe_port = bp.PORT_C
+lower_object_port = bp.PORT_D
+probe_sensor_port = bp.PORT_1
