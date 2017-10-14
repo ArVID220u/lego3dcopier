a7622ce855b107b092b0575d881d20cbeec6f391
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-10-14 15:12:19 +0200

    Add coordinator.py; the main file

---
+++ coordinator.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import scanner
+import printer
+import setup
+import subprocess
+
+
+# The main function, which should initiate the check for a button press, and then start the copying process
+def main():
+    # check for button press
+    button_press = True
+    if button_press:
+        try:
+            copy()
+        finally:
+            # We always want to keep regular position
+            reset()
+
+
+
+# Reset everything into regular position
+def reset():
+    scanner.reset()
+    printer.reset()
+
+
+# The copying procedure
+def copy():
+    # WE ASSUME REGULAR POSITION
+    # start by scanning, which generates a presence matrix
+    presence_matrix = scanner.scan()
+
+    # now, we want to run the legotile algorithm
+    # the legotile algorithm is a cpp program and thus needs to be communicated with using stdin/stdout
+
+    # create a temporary in file which can be sent to the algorithm
+    scanneroutput = "scanneroutput.in"
+    with open(scanneroutput, "w") as scanneroutputfile:
+        # write the presence matrix to it
+        # first, we write the dimensions
+        scanneroutputfile.write(str(presence_matrix[0].size()) + " " + str(presence_matrix.size()) + "\n")
+        # then, we write out the complete matrix
+        for layer in presence_matrix:
+            for row in layer:
+                for item in row:
+                    scanneroutputfile.write(str(item) + " ")
+                scanneroutputfile.write("\n")
+            scanneroutputfile.write("\n")
+        # finished!
+
+    # now send this data to the legotile algorithm and run it
+    legotile_output = ""
+    with open(scanneroutput, "rb") as scanneroutputfile:
+        legotile_output = subprocess.check_output([os.path.abspath(setup.legotile_algorithm_executable)], stdin=scanneroutputfile).decode("utf-8")
+
+    # convert the legotile_output to build instructions
+    # the build instructions will be a list of build_instructions from the printer
+    build_instructions = []
+    for line in legotile_output.strip().split("\n"):
+        rawinstruction = line.strip().split(" ")
+        from printer import Instruction
+        instruction = Instruction(brick=rawinstruction[0], x=int(rawinstruction[1]), y=int(rawinstruction[2]), z=int(rawinstruction[3]), support=[int(x) for x in rawinstruction[4:]])
+        build_instructions.append(instruction)
+
+    # now build these build_instructions
+    printer.build(build_instructions)
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
